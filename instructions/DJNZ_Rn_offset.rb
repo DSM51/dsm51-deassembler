7437033739c8af6b8833e98b4c4ab1c62060a9ad
Damian Rzeszot <damian.rzeszot@gmail.com>
2017-02-05 16:22:07 +0100

    Djnz

--- instructions/DJNZ_Rn_offset.rb
+++ instructions/DJNZ_Rn_offset.rb
@@ -10,4 +10,14 @@ class DJNZ_Rn_offset < Instruction
   def encoding
     ["11011nnn", "offset"]
   end
+
+  def format(pc, *opcodes)
+    n = opcodes[0] & register_n_mask(encoding[0])
+    "djnz R#{n}, #{relative8(opcodes[1], pc+size)}"
+  end
+
+  def jumps(pc, *opcodes)
+    super + [pc+size+signed8(opcodes[1])]
+  end
+
 end
--- instructions/DJNZ_direct_offset.rb
+++ instructions/DJNZ_direct_offset.rb
@@ -10,4 +10,13 @@ class DJNZ_direct_offset < Instruction
   def encoding
     ["11010101", "direct", "offset"]
   end
+
+  def format(pc, *opcodes)
+    "djnz #{direct8(opcodes[1])}, #{relative8(opcode[2], pc+size)}"
+  end
+
+  def jumps(pc, *opcodes)
+    super + [pc+size+signed8(opcodes[2])]
+  end
+
 end

1edd01c570fb5dac96a08688e890705763b4ec6d
Damian Rzeszot <damian.rzeszot@gmail.com>
2017-02-09 20:58:00 +0100

    foramtting

--- decompiler.rb
+++ decompiler.rb
@@ -11,12 +11,26 @@ class Decompiler
   end
 
   def set_responsibility(address, type)
-    responsibilities[address] = type
+    responsibilities[address] ||= []
+    responsibilities[address] << type unless responsibilities[address].include? type
   end
 
-  def responsibility(address)
-    responsibilities[address] || :unknown
+  def unknown?(address)
+    (responsibilities[address] || []).empty?
   end
+
+  def type?(address, type)
+    (responsibilities[address] || []).include? type
+  end
+
+  def code?(address)
+    type? address, :code
+  end
+
+  def jump?(address)
+    type? address, :jump
+  end
+
 
   def results
     @results ||= {}
@@ -30,6 +44,11 @@ class Decompiler
     puts " * start"
     puts " -> entries: #{entries.map { |e| e.hex(16) }}"
 
+    entries.each do |entry|
+      set_responsibility(entry, :code)
+      set_responsibility(entry, :jump)
+    end
+
     checked = []
 
     while !entries.empty? do
@@ -40,8 +59,6 @@ class Decompiler
 
       checked << entry
       checked.uniq!
-
-    #  print " | #{entry.hex(16)}: "
 
       possible = find(entry)
 
@@ -61,11 +78,9 @@ class Decompiler
         set_responsibility(entry+offset, :code)
       end
 
-      # puts " | #{result}"
-
       instruction.jumps(entry, *opcodes).each do |jump|
         entries << jump
-        # puts " |   jump #{jump.hex(16)}"
+        set_responsibility(jump, :jump) unless jump == entry+instruction.size
       end
     end
   end
@@ -85,24 +100,36 @@ class Decompiler
     address = binary.first
 
     loop do
-      print "#{address.hex(16)}"
-
     	value = binary.at(address)
 
-      instruction = results[address]
-
-      if instruction.nil?
-        opcode = take(address, 1).first
-        puts "\t#{opcode.hex(8)}\t(#{responsibility(address)})  '#{text8(opcode)}'"
+      if unknown? address
+        puts "#{address.hex(16)}\t    \t#{value.hex(8)}\t\t\t'#{text8(value)}'"
         address += 1
-      else
+      elsif code? address
+        instruction = results[address]
         opcodes = take(address, instruction.size)
+
         bytes = opcodes.map { |op| op.hex(8) }
         bytes << "  " while bytes.count < 3
 
-      	puts "\t#{bytes.join(' ')}\t\t\t#{instruction.format(address, *opcodes)}"
+        puts "#{address.hex(16)}\t(#{jump?(address)?"jump":"code"})\t#{bytes.join(' ')}\t\t#{instruction.format(address, *opcodes)}"
         address += instruction.size
+      else
+        puts "#{address.hex(16)}\t#{value.hex(8)}\t(wtf)"
+        address += 1
       end
+
+
+      # if instruction.nil?
+      #   opcode = take(address, 1).first
+      #
+      #
+      # else
+      #
+      #
+      #
+      #
+      # end
 
       break if address > binary.last
     end
